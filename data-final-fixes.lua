6ab96121a5a2060cbf4303142ce45832cb798697
Clarence "Sparr" Risher <sparr0@gmail.com>
2019-04-17 23:21:07 -0700

    Update for 0.17, replace strings with #s

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,32 +1,38 @@
 local rich_autoplace = data.raw["map-gen-presets"]["default"]["rich-resources"]["basic_settings"]["autoplace_controls"]
 local rail_autoplace = data.raw["map-gen-presets"]["default"]["rail-world"]["basic_settings"]["autoplace_controls"]
+local ribb_autoplace = data.raw["map-gen-presets"]["default"]["ribbon-world"]["basic_settings"]["autoplace_controls"]
 
 -- ores
 for _,resource in ipairs({
-  "angels-ore5","angels-ore6", -- angelsrefining
-  "clowns-ore1","clowns-ore2","clowns-ore3","clowns-ore4","clowns-ore5","clowns-ore6","clowns-ore7" -- Clowns-Extended-Minerals
+  -- angelsrefining
+  "angels-ore5",
+  "angels-ore6",
+  -- angelspetrochem
+  "angels-natural-gas",
+  -- Clowns-Extended-Minerals
+  "clowns-ore1",
+  "clowns-ore2",
+  "clowns-ore3",
+  "clowns-ore4",
+  "clowns-ore5",
+  "clowns-ore6",
+  "clowns-ore7",
+  "clowns-resource1",
+  "clowns-resource2",
+  -- Angels' seems to intentionally not scale up infinite ores, so we don't do it here
+  -- "infinite-clowns-ore1",
+  -- "infinite-clowns-ore2",
+  -- "infinite-clowns-ore3",
+  -- "infinite-clowns-ore4",
+  -- "infinite-clowns-ore5",
+  -- "infinite-clowns-ore6",
+  -- "infinite-clowns-ore7",
+  -- "infinite-clowns-resource1",
+  -- "infinite-clowns-resource2",
 }) do
   if data.raw["autoplace-control"][resource] then
-    -- if not rich_autoplace[resource] then
-      rich_autoplace[resource] = { richness = "very-good" }
-    -- end
-    -- if not rail_autoplace[resource] then
-      rail_autoplace[resource] = { frequency = "very-low", size = "high" }
-    -- end
-  end
-end
-
--- fluids
-for _,resource in ipairs({
-  "angels-natural-gas", -- angelspetrochem
-  "clowns-resource1","clowns-resource2" -- Clowns-Extended-Minerals
-}) do
-  if data.raw["autoplace-control"][resource] then
-    -- if not rich_autoplace[resource] then
-      rich_autoplace[resource] = { richness = "very-good" }
-    -- end
-    -- if not rail_autoplace[resource] then
-      rail_autoplace[resource] = { frequency = "low", size = "high" }
-    -- end
+    rich_autoplace[resource] = { richness = 2.0 }
+    rail_autoplace[resource] = { frequency = 0.33, size = 3.0 }
+    ribb_autoplace[resource] = { frequency = 3.0, size = 0.5, richness = 2.0 }
   end
 end
